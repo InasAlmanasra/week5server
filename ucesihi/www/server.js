01b07129abce6be2b26033cd4439710e3d7ca18d
Inas Almanasra <i.almanasra.17@ucl.ac.uk>
2018-04-10 12:30:32 -0400

    new w code

--- ucesihi/www/server.js
+++ ucesihi/www/server.js
@@ -1,26 +1,36 @@
 // express is the server that forms part of the nodejs program
-var express = require('express');
-var app = express();
-app.use(function(req, res, next) {
-res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Headers", "X-Requested-With");
-next();
-});
-// serve static files - e.g. html, css
-app.use(express.static(__dirname));
-var https = require('https');
-var fs = require('fs');
-var privateKey = fs.readFileSync('/home/studentuser/certs/client-key.pem').toString();
-var certificate = fs.readFileSync('/home/studentuser/certs/client-cert.pem').toString();
-var credentials = {key: privateKey, cert: certificate};
-var httpsServer = https.createServer(credentials, app);
-httpsServer.listen(4443);
+	var express = require('express');
+	var path = require("path");
+	var app = express();
+	var https = require('https');
+	var fs = require('fs');
 
-<a class="mdl-navigation__link" href="" onclick=" callDivChange ();return false;"><i class="mdlcolor-text--blue-grey-400 material-icons" role="presentation">forum</i>Change Div Content</a>
-app.get('/', function (req, res) {
+	// adding functionality to allow cross-domain queries when PhoneGap is running a server
+	app.use(function(req, res, next) {
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+		res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+		next();
+	});
 
-// run some server-side code
-console.log('the server has received a request');
-res.send('Hello World');
-});
+	
+	// adding functionality to log the requests
+	app.use(function (req, res, next) {
+		var filename = path.basename(req.url);
+		var extension = path.extname(filename);
+		console.log("The file " + filename + " was requested.");
+		next();
+	});
 
+
+	var privateKey = fs.readFileSync('/home/studentuser/certs/client-key.pem').toString();
+	var certificate = fs.readFileSync('/home/studentuser/certs/client-cert.pem').toString(); 
+	var credentials = {key: privateKey, cert: certificate};
+	var httpsServer = https.createServer(credentials, app);
+	httpsServer.listen(4443);
+
+
+
+	// serve static files - e.g. html, css
+	app.use(express.static(__dirname ));
+	
